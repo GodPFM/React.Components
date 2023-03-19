d0137f3b0c43b7977dda74c6b5099312b5160b19
Daniil Asmeev <asmeev.daniil@yandex.ru>
2023-03-19 17:27:40 +0500

    feat: add lorem text into About

--- src/pages/About.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-const About = () => {
-    return React.createElement("div", null, "about page");
-};
-About.propTypes = {};
-export default About;
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import '../styles/About.css';
 
 const About = () => {
-  return <div>about page</div>;
+  return (
+    <div className="about__container">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet excepturi inventore labore
+        sequi unde voluptatem voluptates, voluptatum! Id ipsum laboriosam quidem rerum tenetur,
+        voluptates! Atque autem cumque distinctio dolore fuga modi nihil optio sit unde vitae?
+        Aspernatur delectus dolor iusto minus nam nesciunt quia, quis soluta. Asperiores dignissimos
+        fuga magni.
+      </p>
+    </div>
+  );
 };
-
-About.propTypes = {};
 
 export default About;
