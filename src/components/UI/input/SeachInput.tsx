e50d6c6d5e5232ff29fff4b21df3337c1dab3fb6
Daniil Asmeev <asmeev.daniil@yandex.ru>
2023-03-19 11:55:49 +0500

    feat: add search bar

---
+++ src/components/UI/input/SeachInput.tsx
@@ -0,0 +1,61 @@
+import React, { Component, KeyboardEvent } from 'react';
+import classes from './SeachInput.module.css';
+import PropTypes from 'prop-types';
+
+interface IProps {
+  name?: string;
+}
+interface IState {
+  inputValue: string;
+}
+
+class SeachInput extends Component<IProps, IState> {
+  constructor(props: object) {
+    super(props);
+    this.state = {
+      inputValue: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event: React.FormEvent<HTMLInputElement>) {
+    console.log(this.state);
+    this.setState({ inputValue: event.currentTarget.value });
+  }
+
+  componentDidMount() {
+    const value = localStorage.getItem('searchInput');
+    if (value) {
+      this.setState({ inputValue: value });
+    } else {
+      this.setState({ inputValue: '' });
+    }
+    window.addEventListener('beforeunload', this.handleWindowBeforeUnload);
+  }
+
+  handleWindowBeforeUnload = () => {
+    const value = this.state.inputValue;
+    localStorage.setItem('searchInput', value);
+  };
+
+  componentWillUnmount() {
+    const value = this.state.inputValue;
+    window.removeEventListener('beforeunload', this.handleWindowBeforeUnload);
+    localStorage.setItem('searchInput', value);
+  }
+
+  render() {
+    return (
+      <div className={classes.searchInput__container}>
+        <input
+          className={classes.searchInput}
+          value={this.state.inputValue}
+          onInput={this.handleChange}
+          placeholder="Search"
+          type="text"
+        />
+      </div>
+    );
+  }
+}
+export default SeachInput;
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import '../styles/MainPage.css';
 import PropTypes from 'prop-types';
+import SeachInput from '../components/UI/input/SeachInput';
 
 const MainPage = () => {
-  return <div className="mainPage">Main page</div>;
+  return (
+    <div className="mainPage">
+      <SeachInput />
+    </div>
+  );
 };
 
 MainPage.propTypes = {};
