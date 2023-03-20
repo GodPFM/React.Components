044d591a177f61f978ccfaf88cbf6363d956ffc0
Daniil Asmeev <asmeev.daniil@yandex.ru>
2023-03-20 16:54:33 +0500

    feat: add show current page on header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import './Header.css';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
   return (
     <header className="header">
       <div className="header__wrapper">
         <nav className="header__nav">
-          <Link className="header__link" to="/">
+          <NavLink
+            className={({ isActive }) => (isActive ? 'header__link active' : 'header__link')}
+            to="/"
+          >
             Main page
-          </Link>
-          <Link className="header__link" to="/about">
+          </NavLink>
+          <NavLink
+            className={({ isActive }) => (isActive ? 'header__link active' : 'header__link')}
+            to="/about"
+          >
             About us
-          </Link>
+          </NavLink>
         </nav>
       </div>
     </header>
