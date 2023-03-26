3bdbf5478826f844b9cd1cd19073ce41e0b56be0
Daniil Asmeev <asmeev.daniil@yandex.ru>
2023-03-26 17:17:36 +0500

    feat: add display error message

--- src/pages/AddCardForm.tsx
+++ src/pages/AddCardForm.tsx
@@ -6,12 +6,23 @@ import Checkbox from '../components/UI/checkbox/Checkbox';
 import Radiobutton from '../components/UI/radiobutton/Radiobutton';
 import ImageInput from '../components/UI/imageInput/ImageInput';
 import Button from '../components/UI/button/Button';
+import ValidationInputs from '../utils/Validation';
+import ErrorMessage from '../components/UI/errorMessage/ErrorMessage';
 
 interface IProps {
   test?: string;
 }
 
-class AddCardForm extends Component {
+interface IState {
+  textCheck?: string | boolean;
+  dateCheck?: string | boolean;
+  selectCheck?: string | boolean;
+  checkboxCheck?: string | boolean;
+  radioCheck?: string | boolean;
+  imageInputCheck?: string | boolean;
+}
+
+class AddCardForm extends Component<IProps, IState> {
   private titleRef = React.createRef<HTMLInputElement>();
   private dateRef = React.createRef<HTMLInputElement>();
   private selectRef = React.createRef<HTMLSelectElement>();
@@ -22,15 +33,23 @@ class AddCardForm extends Component {
   constructor(props: IProps) {
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  componentDidMount() {
-    console.log(this.titleRef?.current);
+    this.state = {};
   }
 
   handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
-    console.log(this.selectRef.current?.value);
+    console.log(this.dateRef.current?.value);
+    this.setState({
+      textCheck: ValidationInputs.validateInputText(this.titleRef.current?.value),
+      dateCheck: ValidationInputs.validateInputDate(this.dateRef.current?.value),
+      selectCheck: ValidationInputs.validateSelect(this.selectRef.current?.value),
+      checkboxCheck: ValidationInputs.validateCheckbox(this.checkboxRef.current?.checked),
+      radioCheck: ValidationInputs.validateRadioInput(
+        this.radioWomanRef.current?.checked,
+        this.radioMaleRef.current?.checked
+      ),
+      imageInputCheck: ValidationInputs.validateImageInput(this.imageLoadRef.current?.value),
+    });
   }
   render() {
     return (
@@ -43,6 +62,7 @@ class AddCardForm extends Component {
             name={'title'}
             inputRef={this.titleRef}
           />
+          {this.state.textCheck && <ErrorMessage text={this.state.textCheck} />}
           <Input
             placeholder={'Date'}
             type={'date'}
@@ -50,12 +70,15 @@ class AddCardForm extends Component {
             name={'date'}
             inputRef={this.dateRef}
           />
+          {this.state.dateCheck && <ErrorMessage text={this.state.dateCheck} />}
           <Select selectRef={this.selectRef} />
+          {this.state.selectCheck && <ErrorMessage text={this.state.selectCheck} />}
           <Checkbox
             value={'terms'}
             text={'I accept terms and conditions'}
             checkboxRef={this.checkboxRef}
           />
+          {this.state.checkboxCheck && <ErrorMessage text={this.state.checkboxCheck} />}
           <Radiobutton
             values={[
               ['Male', this.radioMaleRef],
@@ -64,7 +87,9 @@ class AddCardForm extends Component {
             name={'sex'}
             title={'Sex:'}
           />
+          {this.state.radioCheck && <ErrorMessage text={this.state.radioCheck} />}
           <ImageInput imageInputRef={this.imageLoadRef} />
+          {this.state.imageInputCheck && <ErrorMessage text={this.state.imageInputCheck} />}
           <Button text={'Submit'} />
         </form>
       </div>
---
+++ src/utils/Validation.ts
@@ -0,0 +1,50 @@
+export default class ValidationInputs {
+  static validateInputText(value: string | undefined) {
+    if (!value) {
+      return 'Value is empty';
+    }
+    if (value.length < 3) {
+      return 'Length < 3';
+    }
+    return false;
+  }
+
+  static validateInputDate(value: string | undefined) {
+    const dateNow = Date.now();
+    if (!value) {
+      return 'Field is empty';
+    }
+    const dateValue = new Date(value);
+    if (Number(dateValue) - Number(dateNow) < 0) {
+      return 'The date has passed';
+    }
+    return false;
+  }
+  static validateSelect(value: string | undefined) {
+    if (!value) {
+      return 'Choose category';
+    }
+    return false;
+  }
+
+  static validateCheckbox(value: boolean | undefined) {
+    if (!value) {
+      return 'Accept the agreement';
+    }
+    return false;
+  }
+
+  static validateRadioInput(...args: (boolean | undefined)[]) {
+    if (!args.includes(true)) {
+      return 'Choose your sex';
+    }
+    return false;
+  }
+
+  static validateImageInput(value: string | undefined) {
+    if (!value) {
+      return 'Upload image';
+    }
+    return false;
+  }
+}
