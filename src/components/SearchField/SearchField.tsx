2a5364a855e3e4398ab2243283cb9671df11a8b4
Daniil Asmeev <asmeev.daniil@yandex.ru>
2023-04-01 21:37:18 +0500

    refactor: change classes SearchField and Button to functional component

--- src/components/SearchField/SearchField.tsx
+++ src/components/SearchField/SearchField.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import Button from '../UI/button/Button';
 import Input from '../UI/input/Input';
@@ -7,17 +7,15 @@ interface IProps {
   openModal: () => void;
 }
 
-class SearchField extends Component<IProps> {
-  render() {
-    return (
-      <div className="search__container">
-        <Link className="search__create-link" to={'add-item'} onClick={this.props.openModal}>
-          <Button text={'Create card'} />
-        </Link>
-        <Input name={'search'} placeholder={'Search'} isNeedSave={true} />
-      </div>
-    );
-  }
-}
+const SearchField = (props: IProps) => {
+  return (
+    <div className="search__container">
+      <Link className="search__create-link" to={'add-item'} onClick={props.openModal}>
+        <Button text={'Create card'} />
+      </Link>
+      <Input name={'search'} placeholder={'Search'} isNeedSave={true} />
+    </div>
+  );
+};
 
 export default SearchField;
--- src/components/UI/button/Button.tsx
+++ src/components/UI/button/Button.tsx
@@ -1,29 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import classes from './Button.module.css';
-
-interface IState {
-  test?: string;
-}
 
 interface IProps {
   text: string;
   onClck?: (e: React.MouseEvent) => void;
 }
 
-class Button extends Component<IProps, IState> {
-  constructor(props: IProps) {
-    super(props);
-    this.state = {};
-  }
-  render() {
-    return (
-      <div className={classes.button__container}>
-        <button className={classes.button} onClick={this.props.onClck}>
-          {this.props.text}
-        </button>
-      </div>
-    );
-  }
-}
+const Button = (props: IProps) => {
+  return (
+    <div className={classes.button__container}>
+      <button className={classes.button} onClick={props.onClck}>
+        {props.text}
+      </button>
+    </div>
+  );
+};
 
 export default Button;
