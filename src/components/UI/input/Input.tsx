6ffec92e7989d9c461af0c32e801ec0b43d77c57
Daniil Asmeev <asmeev.daniil@yandex.ru>
2023-04-01 22:06:24 +0500

    refactor: change class Input to functional component

--- src/components/UI/input/Input.tsx
+++ src/components/UI/input/Input.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import classes from './Input.module.css';
 
 interface IProps {
@@ -9,70 +9,59 @@ interface IProps {
   type?: string;
   inputRef?: React.Ref<HTMLInputElement> | null | undefined;
 }
-interface IState {
-  inputValue: string;
-}
 
-class Input extends Component<IProps, IState> {
-  private type: string;
-  constructor(props: IProps) {
-    super(props);
-    this.type = this.props.type ? this.props.type : 'text';
-    this.state = {
-      inputValue: '',
-    };
-    this.handleChange = this.handleChange.bind(this);
-  }
+const Input = (props: IProps) => {
+  const [inputValue, setInputValue] = useState('');
 
-  handleChange(event: React.FormEvent<HTMLInputElement>) {
-    this.setState({ inputValue: event.currentTarget.value });
-  }
-
-  componentDidMount() {
-    if (this.props.isNeedSave) {
-      const value = localStorage.getItem(`${this.props.name}Input`);
+  useEffect(() => {
+    if (props.isNeedSave) {
+      const value = localStorage.getItem(`${props.name}Input`);
       if (value) {
-        this.setState({ inputValue: value });
+        setInputValue(value);
       } else {
-        this.setState({ inputValue: '' });
+        setInputValue('');
       }
-      window.addEventListener('beforeunload', this.handleWindowBeforeUnload);
+      window.addEventListener('beforeunload', handleWindowBeforeUnload);
     }
-  }
+  }, []);
 
-  handleWindowBeforeUnload = () => {
-    if (this.props.isNeedSave) {
-      const value = this.state.inputValue;
-      localStorage.setItem(`${this.props.name}Input`, value);
+  useEffect(() => {
+    return () => {
+      if (props.isNeedSave) {
+        console.log(inputValue);
+        window.removeEventListener('beforeunload', handleWindowBeforeUnload);
+        localStorage.setItem(`${props.name}Input`, inputValue);
+      }
+    };
+  });
+
+  const type = props.type ? props.type : 'text';
+
+  const handleWindowBeforeUnload = () => {
+    if (props.isNeedSave) {
+      localStorage.setItem(`${props.name}Input`, inputValue);
     }
   };
 
-  componentWillUnmount() {
-    if (this.props.isNeedSave) {
-      const value = this.state.inputValue;
-      window.removeEventListener('beforeunload', this.handleWindowBeforeUnload);
-      localStorage.setItem(`${this.props.name}Input`, value);
-    }
-  }
+  const handleChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setInputValue(event.currentTarget.value);
+  };
 
-  render() {
-    return (
-      <div className={classes.searchInput__container}>
-        {this.props.labelForInput && (
-          <label htmlFor={this.props.name}>{this.props.labelForInput}</label>
-        )}
-        <input
-          className={classes.searchInput}
-          ref={this.props.inputRef}
-          value={this.state.inputValue}
-          onInput={this.handleChange}
-          placeholder={this.props.placeholder}
-          name={this.props.name}
-          id={this.props.name}
-          type={this.type}
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div className={classes.searchInput__container}>
+      {props.labelForInput && <label htmlFor={props.name}>{props.labelForInput}</label>}
+      <input
+        className={classes.searchInput}
+        ref={props.inputRef}
+        value={inputValue}
+        onInput={handleChange}
+        placeholder={props.placeholder}
+        name={props.name}
+        id={props.name}
+        type={type}
+      />
+    </div>
+  );
+};
+
 export default Input;
