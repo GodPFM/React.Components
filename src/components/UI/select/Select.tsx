f1dcd890b24f7783cf44281e9d76397a4d5c392a
Daniil Asmeev <asmeev.daniil@yandex.ru>
2023-04-02 19:01:55 +0500

    refactor: change class Select to functional component

--- src/components/UI/select/Select.tsx
+++ src/components/UI/select/Select.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import classes from './Select.module.css';
 import { UseFormRegister } from 'react-hook-form';
 import { CardFormData } from '../../../types/CardForm';
@@ -7,26 +7,24 @@ interface IProps {
   register: UseFormRegister<CardFormData>;
 }
 
-class Select extends Component<IProps> {
-  render() {
-    return (
-      <select
-        className={classes.select}
-        id="profession"
-        defaultValue=""
-        {...this.props.register('professionSelect', {
-          required: 'Select one option',
-        })}
-      >
-        <option value="" disabled>
-          Choose your profession
-        </option>
-        <option value="programmer">Programmer</option>
-        <option value="designer">Designer</option>
-        <option value="other">Other</option>
-      </select>
-    );
-  }
-}
+const Select = (props: IProps) => {
+  return (
+    <select
+      className={classes.select}
+      id="profession"
+      defaultValue=""
+      {...props.register('professionSelect', {
+        required: 'Select one option',
+      })}
+    >
+      <option value="" disabled>
+        Choose your profession
+      </option>
+      <option value="programmer">Programmer</option>
+      <option value="designer">Designer</option>
+      <option value="other">Other</option>
+    </select>
+  );
+};
 
 export default Select;
