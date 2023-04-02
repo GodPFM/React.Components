dc289b24d1c6d79ab2df1493934894a111efc5d3
Daniil Asmeev <asmeev.daniil@yandex.ru>
2023-04-02 08:40:23 +0500

    refactor: change class AddCardForm to functional component

--- src/pages/AddCardForm.tsx
+++ src/pages/AddCardForm.tsx
@@ -1,33 +1,23 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import '../styles/AddCardForm.css';
-import { CardFormCheck } from '../types/CardForm';
 import CardsWithUsers from '../components/CardsWithUsers/CardsWithUsers';
 import Form from '../components/Form/Form';
 import { CardWithUsers } from '../types/CardWithUsers';
 
-class AddCardForm extends Component<object, CardFormCheck> {
-  constructor(props: object) {
-    super(props);
-    this.setCard = this.setCard.bind(this);
-    this.state = {
-      cards: [],
-      openModal: false,
-    };
-  }
+const AddCardForm = () => {
+  const [cards, setCards] = useState<CardWithUsers[]>([]);
 
-  async setCard(objectData: CardWithUsers) {
-    await this.setState({ cards: [...this.state.cards, objectData] });
-  }
+  const setCard = (objectData: CardWithUsers) => {
+    setCards([...cards, objectData]);
+  };
 
-  render() {
-    return (
-      <div className="create-card__container">
-        <h2 className="create-card__header">Create card</h2>
-        <Form setCard={this.setCard} />
-        <CardsWithUsers cards={this.state.cards} />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="create-card__container">
+      <h2 className="create-card__header">Create card</h2>
+      <Form setCard={setCard} />
+      <CardsWithUsers cards={cards} />
+    </div>
+  );
+};
 
 export default AddCardForm;
