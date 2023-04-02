e362484311f2f3f77571349329037956744faa8b
Daniil Asmeev <asmeev.daniil@yandex.ru>
2023-04-02 18:30:38 +0500

    refactor: change class ImageInput to functional component

--- src/components/UI/imageInput/ImageInput.tsx
+++ src/components/UI/imageInput/ImageInput.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import classes from './ImageInput.module.css';
 import { UseFormRegister } from 'react-hook-form';
 import ValidationInputs from '../../../utils/Validation';
@@ -9,28 +9,26 @@ interface IProps {
   getImage: (file: FileList) => void;
 }
 
-class ImageInput extends Component<IProps> {
-  render() {
-    return (
-      <div className={classes.imageInput__wrapper}>
-        <div>
-          <label className={classes.imageInput__container}>
-            <input
-              type="file"
-              className={classes.imageInput__input}
-              accept="image/png, image/jpg, image/jpeg, image/gif"
-              {...this.props.imageInputRef('inputImage', {
-                validate: (data) => {
-                  const validate = ValidationInputs.validateImageInput(data as unknown as FileList);
-                  return validate ? validate : true;
-                },
-              })}
-            />
-          </label>
-        </div>
+const ImageInput = (props: IProps) => {
+  return (
+    <div className={classes.imageInput__wrapper}>
+      <div>
+        <label className={classes.imageInput__container}>
+          <input
+            type="file"
+            className={classes.imageInput__input}
+            accept="image/png, image/jpg, image/jpeg, image/gif"
+            {...props.imageInputRef('inputImage', {
+              validate: (data) => {
+                const validate = ValidationInputs.validateImageInput(data as unknown as FileList);
+                return validate ? validate : true;
+              },
+            })}
+          />
+        </label>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default ImageInput;
