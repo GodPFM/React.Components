81fe05048d33bc7cbe74245272238d08d038ce00
Daniil Asmeev <asmeev.daniil@yandex.ru>
2023-04-02 14:21:41 +0500

    refactor: change class UserCard to functional component

--- src/components/UI/UserCard/UserCard.tsx
+++ src/components/UI/UserCard/UserCard.tsx
@@ -1,24 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import classes from './UserCard.module.css';
 import { CardWithUsers } from '../../../types/CardWithUsers';
 
 interface IProps {
   card: CardWithUsers;
 }
-class UserCard extends Component<IProps> {
-  render() {
-    return (
-      <div className={classes.item}>
-        <img className={classes.item__image} src={this.props.card.image} alt="Product image" />
-        <h2 className={classes.item__title}>{this.props.card.name}</h2>
-        <p>Birthday: {this.props.card.date}</p>
-        <p>Profession: {this.props.card.profession}</p>
-        <p>Sex: {this.props.card.sex}</p>
-        <p>Accepted terms</p>
-        {this.props.card.agreeGetNotification && <p>Agree to get notifications</p>}
-      </div>
-    );
-  }
-}
+
+const UserCard = (props: IProps) => {
+  return (
+    <div className={classes.item}>
+      <img className={classes.item__image} src={props.card.image} alt="Product image" />
+      <h2 className={classes.item__title}>{props.card.name}</h2>
+      <p>Birthday: {props.card.date}</p>
+      <p>Profession: {props.card.profession}</p>
+      <p>Sex: {props.card.sex}</p>
+      <p>Accepted terms</p>
+      {props.card.agreeGetNotification && <p>Agree to get notifications</p>}
+    </div>
+  );
+};
 
 export default UserCard;
