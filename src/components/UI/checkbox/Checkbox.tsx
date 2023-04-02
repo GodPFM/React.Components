a76e1b3d3499534a63f4d190cee73cdb32cb973d
Daniil Asmeev <asmeev.daniil@yandex.ru>
2023-04-02 18:29:37 +0500

    refactor: change class Checkbox to functional component

--- src/components/UI/checkbox/Checkbox.tsx
+++ src/components/UI/checkbox/Checkbox.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import classes from './Checkbox.module.css';
 import { RegisterOptions, UseFormRegister } from 'react-hook-form';
 import { CardFormData } from '../../../types/CardForm';
@@ -10,23 +10,21 @@ interface IProps {
   required?: RegisterOptions | undefined;
 }
 
-class Checkbox extends Component<IProps> {
-  render() {
-    return (
-      <div className={classes.checkbox__container}>
-        <input
-          type="checkbox"
-          id={this.props.value}
-          value={this.props.value}
-          {...this.props.checkboxRef(`${this.props.value}Checkbox`, this.props.required)}
-        />
-        <label className={classes.checkbox__label} htmlFor={this.props.value}>
-          <span className={classes.checkbox__span}></span>
-          {this.props.text}
-        </label>
-      </div>
-    );
-  }
-}
+const Checkbox = (props: IProps) => {
+  return (
+    <div className={classes.checkbox__container}>
+      <input
+        type="checkbox"
+        id={props.value}
+        value={props.value}
+        {...props.checkboxRef(`${props.value}Checkbox`, props.required)}
+      />
+      <label className={classes.checkbox__label} htmlFor={props.value}>
+        <span className={classes.checkbox__span}></span>
+        {props.text}
+      </label>
+    </div>
+  );
+};
 
 export default Checkbox;
