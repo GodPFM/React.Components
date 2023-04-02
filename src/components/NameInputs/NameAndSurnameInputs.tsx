ff6a9a7b4ca90a09ec2def0942b846d283635e57
Daniil Asmeev <asmeev.daniil@yandex.ru>
2023-04-02 18:57:58 +0500

    refactor: change class NameAndSurnameInputs to functional component

--- src/components/NameInputs/NameAndSurnameInputs.tsx
+++ src/components/NameInputs/NameAndSurnameInputs.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ErrorMessage from '../UI/errorMessage/ErrorMessage';
 import classes from './NameAndSurname.module.css';
 import FormInput from '../UI/formInput/FormInput';
@@ -12,49 +12,47 @@ interface IProps {
   surnameError: string | undefined;
 }
 
-class NameAndSurnameInputs extends Component<IProps> {
-  render() {
-    return (
-      <div className={classes.container}>
-        <div>
-          <FormInput
-            placeholder={'Name'}
-            labelForInput={'Name:'}
-            name={'name'}
-            type={'text'}
-            register={this.props.nameRef}
-            validate={{
-              required: 'Value is empty',
-              minLength: { value: 3, message: 'Length < 3' },
-              pattern: {
-                value: /^[A-Z]/,
-                message: 'First letter must be uppercase',
-              },
-            }}
-          />
-          {this.props.nameError && <ErrorMessage text={this.props.nameError} />}
-        </div>
-        <div>
-          <FormInput
-            placeholder={'Surname'}
-            labelForInput={'Surname:'}
-            name={'surname'}
-            type={'text'}
-            register={this.props.surnameRef}
-            validate={{
-              required: 'Value is empty',
-              minLength: { value: 3, message: 'Length < 3' },
-              pattern: {
-                value: /^[A-Z]/,
-                message: 'First letter must be uppercase',
-              },
-            }}
-          />
-          {this.props.surnameError && <ErrorMessage text={this.props.surnameError} />}
-        </div>
+const NameAndSurnameInputs = (props: IProps) => {
+  return (
+    <div className={classes.container}>
+      <div>
+        <FormInput
+          placeholder={'Name'}
+          labelForInput={'Name:'}
+          name={'name'}
+          type={'text'}
+          register={props.nameRef}
+          validate={{
+            required: 'Value is empty',
+            minLength: { value: 3, message: 'Length < 3' },
+            pattern: {
+              value: /^[A-Z]/,
+              message: 'First letter must be uppercase',
+            },
+          }}
+        />
+        {props.nameError && <ErrorMessage text={props.nameError} />}
       </div>
-    );
-  }
-}
+      <div>
+        <FormInput
+          placeholder={'Surname'}
+          labelForInput={'Surname:'}
+          name={'surname'}
+          type={'text'}
+          register={props.surnameRef}
+          validate={{
+            required: 'Value is empty',
+            minLength: { value: 3, message: 'Length < 3' },
+            pattern: {
+              value: /^[A-Z]/,
+              message: 'First letter must be uppercase',
+            },
+          }}
+        />
+        {props.surnameError && <ErrorMessage text={props.surnameError} />}
+      </div>
+    </div>
+  );
+};
 
 export default NameAndSurnameInputs;
