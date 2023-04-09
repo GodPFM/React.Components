a8f5b02ab0f7e1ad7691124941f308d65bfb9c03
Daniil Asmeev <asmeev.daniil@yandex.ru>
2023-04-09 17:13:14 +0500

    feat: add scroll lock on open modal window

--- src/components/UI/Modal/Modal.tsx
+++ src/components/UI/Modal/Modal.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useLayoutEffect, useRef } from 'react';
 import classes from './Modal.module.css';
+import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock';
 
 interface IProps {
   children: React.ReactNode;
@@ -7,11 +8,25 @@ interface IProps {
 }
 
 const Modal = (props: IProps) => {
+  const targetRef = useRef<HTMLDivElement>(null);
+  let targetElement: Element | null = null;
+
+  useLayoutEffect(() => {
+    targetElement = targetRef.current;
+    if (targetElement) {
+      disableBodyScroll(targetElement);
+    }
+    return () => {
+      if (targetElement) {
+        enableBodyScroll(targetElement);
+      }
+    };
+  }, []);
   const handleClick = (e: React.MouseEvent) => {
     e.stopPropagation();
   };
   return (
-    <div className={classes.modal} onClick={props.closeModal}>
+    <div ref={targetRef} className={classes.modal} onClick={props.closeModal}>
       <div className={classes.modal__container} onClick={handleClick}>
         {props.children}
       </div>
