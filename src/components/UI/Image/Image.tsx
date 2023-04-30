beec1fdb514450a16bd2d7390239ac35160574ab
Daniil Asmeev <asmeev.daniil@yandex.ru>
2023-05-01 01:58:12 +0500

    fix: infinity load on ssr images

--- src/components/UI/Image/Image.tsx
+++ src/components/UI/Image/Image.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import classes from './Image.module.css';
 import Loader from '../loading/Loader';
 
@@ -7,7 +7,13 @@ interface IProps {
 }
 
 const Image = (props: IProps) => {
+  const imgRef = useRef<HTMLImageElement>(null);
   const [isImageLoad, setIsImageLoad] = useState(false);
+  useEffect(() => {
+    if (imgRef.current?.complete) {
+      loadImageHandler();
+    }
+  }, []);
   const loadImageHandler = () => {
     setIsImageLoad(true);
   };
@@ -23,6 +29,7 @@ const Image = (props: IProps) => {
       <img
         className={`${classes.image} ${hideImage}`}
         src={props.src}
+        ref={imgRef}
         alt="Product image"
         onLoad={loadImageHandler}
         data-testid="image"
